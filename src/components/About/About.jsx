277f9a37aa3aa47760d2a544e42a8ac1ec094f11
Christian Araneda <ignacio.aranedaocares@gmail.com>
2023-07-06 22:37:54 -0400

    modificaciones about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,17 @@
-// import style from "./About.module.css";
+import style from "./About.module.css";
+import fotoPerfil from "./Christian Ignacio Araneda Ocares .jpeg";
 
 const About = () => {
   return (
     <div>
-      <h1>Ignacio Araneda</h1>
+      <a href="https://www.linkedin.com/in/christian-araneda/">
+        <h1 className={style.nombre}>Christian Ignacio Araneda</h1>
+        <img
+          className={style.imgen}
+          src={fotoPerfil}
+          alt="Christian Ignacio Araneda"
+        />
+      </a>
     </div>
   );
 };
